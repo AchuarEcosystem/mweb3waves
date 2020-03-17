402d423eedc529291a5d950157acd6461f2322e1
AchuarEcosystem <meyzenblr@mail.ru>
2020-03-17 23:33:54 +0300

    '12'

--- src/components/app.js
+++ src/components/app.js
@@ -71,7 +71,7 @@ class App extends React.Component {
                                 {type:"integer", value:this.state.purchase.userDiscount},
                                 {type:"integer", value:this.state.purchase.orderQty},
                                 //{type:"integer", value:this.state.purchase.amount*this.wavelet}
-                                {type:"integer", value:(100-this.state.purchase.userDiscount)*this.state.purchase.orderQty*1*this.wavelet/100}
+                                {type:"integer", value:(100-this.state.purchase.userDiscount)*this.state.purchase.orderQty*10*this.wavelet/100}
                             ]
                         }, payment: [{assetId: "WAVES", amount:this.state.purchase.amount*this.wavelet}]
                 }
@@ -103,8 +103,9 @@ class App extends React.Component {
                         <input className="form-control" type="number" step="0.5" min="0" max="12" placeholder="Discount" onChange={(e) => this.updateValue("purchase", "userDiscount", e.target.value)}/>
                         <br></br>
                         <input className="form-control" type="number" step="1" min="1" max="1000" placeholder="Order Qty" onChange={(e) => this.updateValue("purchase", "orderQty", e.target.value)}/>
+                        
                         <br></br> 
-                        <a className="form-text text-muted" target="_blank" href={this.explorerUrl + "/tx/" + this.state.purchase.dataid}>Transaction / Total a Pagar: {(100-this.state.purchase.userDiscount)*this.state.purchase.orderQty*1*this.wavelet/100}</a>
+                        <a className="form-text text-muted" target="_blank" href={this.explorerUrl + "/tx/" + this.state.purchase.txid}>Transaction: {(100-this.state.purchase.userDiscount)*this.state.purchase.orderQty*10*this.wavelet/100}</a>
                         <br></br>
                         <input className="btn btn-primary" type="submit" value="Buy aBitCO2 tokens" onClick={this.purchase}/>
                         <br></br>
