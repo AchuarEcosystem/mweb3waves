17ecae2de0aa9f64136498d98011cd83c81cee38
AchuarEcosystem <meyzenblr@mail.ru>
2020-03-18 10:28:18 +0300

    '3'

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ class App extends React.Component {
                 userData: '',
                 userDiscount: '',
                 orderQty: '',
-                amount: '',
+                //amount: '',
                 txid: ''
               }
         };
